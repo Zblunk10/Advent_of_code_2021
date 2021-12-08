c3dbef97441f7627b05a89cb81f7f68db8103232
Michaela Charvatova <zblunk10@gmail.com>
2021-12-08 08:22:38 +0100

    Day 5, part 1

---
+++ 05/Advent_05_01.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+
+with open("05/input.txt", "r") as f:
+  file_content = f.read()
+
+# splits instructions
+file_content = file_content.split("\n")
+file_content = [x.split(" -> ") for x in file_content]
+
+
+instructions = []
+for x in file_content:
+  left = [int(x) for x in x[0].split(",")]
+  right = [int(x) for x in x[1].split(",")]
+  instructions.append([left[0], left[1], right[0], right[1]])
+#print(instructions)
+
+
+field = pd.DataFrame(np.zeros((1000,1000)))
+# field.loc[3:3, 5:7] = 6
+# print(field.loc[0,0])
+# print(field)
+
+for instruction in instructions:
+  start_column = instruction[0]
+  start_row = instruction[1]
+  end_column=instruction[2]
+  end_row = instruction[3]
+  rows = [start_row, end_row]
+  columns = [start_column, end_column]
+
+  # only take into account horizontal/vertical
+  if start_column == end_column or start_row == end_row:
+    field.loc[min(rows):max(rows), min(columns):max(columns)] +=1
+print((field))
+x = field[field>1].count()
+print(x.sum())
+
+
